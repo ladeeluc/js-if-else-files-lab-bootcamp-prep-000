708c7d8c716de7bdd9c58dfaed6dd899e433ee7d
secure-torrent-6655 <laura.hatch.lh@gmail.com>
2019-05-29 03:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
 name = "Susan"
-if (na(me ==="Susan") {
+if (name ==="Susan") {
   console.log ("The name is correct")
 }else {
   console.log("Expected the name to be Susan")
