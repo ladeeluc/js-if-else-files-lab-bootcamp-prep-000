64b2c2d7d3ce9ac1fcc22d9f13a64b6b3c80ca46
secure-torrent-6655 <laura.hatch.lh@gmail.com>
2019-05-29 03:47:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
-
+name = "Susan"
+if (na(me ==="Susan") {
+  console.log ("The name is correct")
+}else {
+  console.log("Expected the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name, height}
 ss
